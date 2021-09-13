89189267e9baab51a3d84fffc106056cb9d05010
Ver33d <sergei33dd@mail.ru>
2021-09-13 13:27:36 +0300

    made some improvements
    Refactoring

--- Chess+.cpp
+++ Chess+.cpp
@@ -6,8 +6,8 @@ int main(int argc, char* argv[]) {
     argv[1] = (char*)"null"; 
  if(Figure::deskout(argv[1]))  {  
    std::cout << "Correct " << argv[1];  
-   std::cout << " to " << (argv[1]=(char*)"a1")<<" Default\n";   
-   std::cout << "Usage example: chess+ e5\n"; 
+   std::cout << " to " << (argv[1]=(char*)"a1")<<" Default"<<endl;   
+   std::cout << "Usage example: chess+ e5<<endl"; 
 } 
 
 Prince f(argv[1]);   
