bb43ead7c83ba23a3c6435ae3c455524d745e3c6
Ver33d <sergei33dd@mail.ru>
2021-09-15 17:42:55 +0300

    Update Chess+.cpp
    Refactoring

--- Chess+.cpp
+++ Chess+.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]) {
  if(argc < 2)   
-    argv[1] = (char*)"null"; 
+   argv[1] = (char*)"null"; 
  if(Figure::deskout(argv[1]))  {  
    std::cout << "Correct " << argv[1];  
    std::cout << " to " << (argv[1]=(char*)"a1")<<" Default"<<endl;   
