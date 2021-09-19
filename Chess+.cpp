7aed73339cff470b9a82b0d60b0904e34671df48
Ver33d <sergei33dd@mail.ru>
2021-09-19 15:02:45 +0300

    made some improvements
    Refactoring

--- Chess+.cpp
+++ Chess+.cpp
@@ -3,12 +3,13 @@
 
 int main(int argc, char* argv[]) {
  if(argc < 2)   
-   argv[1] = (char*)"null"; 
- if(Figure::deskout(argv[1]))  {  
+   argv[1] = (char*)"null" ; 
+ if(Figure::deskout(argv[1]))  
+   {  
    std::cout << "Correct " << argv[1];  
    std::cout << " to " << (argv[1]=(char*)"a1")<<" Default"<<endl;   
    std::cout << "Usage example: chess+ e5<<endl"; 
-} 
+   } 
 Prince f(argv[1]);   
 do {  
    f.danger();  
